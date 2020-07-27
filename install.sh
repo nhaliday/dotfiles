0d4cd562f03e2219b23c163d8d4b643fc1009d47
Nick Haliday <nick@Nicks-MBP-2020.fios-router.home>
2020-07-27 13:22:25 -0400

    .

--- install.sh
+++ install.sh
@@ -7,18 +7,16 @@ pushd "$DOTFILES_DIR"
 
 mkdir -p $HOME/.config $HOME/.vim/plugged $HOME/.vim/autoload $HOME/.vim/backupfiles $HOME/.vim/swapfiles $HOME/.vim/undofiles
 
-## Xcode
 # DO: Install Xcode in app store
 # Command-line tools
 if ! output=$(xcode-select --install || true); then
     # In case where they're already installed, the exit code is 1, but this script doesn't consider that to be an error case, so we work around it.
-    if ! echo $output | rg 'already installed'; then
+    if ! echo "$output" | rg 'already installed'; then
        # Fail with the same error
        xcode-select --install
     fi
 fi
 
-## Homebrew
 [[ -f /usr/local/bin/brew ]] || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 # First time I installed the Freedom app cask I had to add an environment variable: https://apple.stackexchange.com/questions/393481/homebrew-cask-download-failure-ssl-certificate-problem-certificate-has-expired
 brew bundle install --no-lock
@@ -30,7 +28,7 @@ stow --targe=$HOME hammerspoon vim
 yes | /usr/local/opt/fzf/install
 
 if ! output=$(yes N | rbenv install 2.7.1 || true); then
-	if ! echo $output | rg 'already exists'; then
+	if ! echo "$output" | rg 'already exists'; then
 		rbenv install 2.7.1
 	fi
 fi
@@ -40,5 +38,8 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 vim +PlugUpgrade +PlugInstall +PlugUpdate +qall
+
+# DO: Install Cisco AnyConnect proxy
+# DO: Copy over ~/Library/Application\ Support/BetterTouchTool
 
 popd
